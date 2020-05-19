10d47f751bca67f8b3d7af1c919e56df9f6cdc3a
Justin McKetney <justinmcketney@gmail.com>
2020-05-19 15:14:39 -0500

    results from some of the multilabel grid search

--- CHTC_parallel_grid_search/make_params_short_list.py
+++ CHTC_parallel_grid_search/make_params_short_list.py
@@ -8,7 +8,7 @@ import pandas as pd
 gscRFR = GridSearchCV(
     estimator=RandomForestRegressor(),
     param_grid={
-        'max_depth': (5, 10), #15, 20, 25, None), #, 30, 40, 50, 60, 80, 100, 150),
+        'max_depth': (5, 10, 20), #15, 20, 25, None), #, 30, 40, 50, 60, 80, 100, 150),
         'n_estimators': (100, 200), #500, 1000), #, 4000, 8000),
         'min_samples_leaf': (1, 5), #10, 20, 40, 100), #, 200, 500, 700, 1000),
         'min_samples_split': (2, 5), #10, 20, 30, 40),#, 50, 100, 150, 300, 500, 1000),
@@ -25,3 +25,5 @@ param_list = list(ParameterGrid(gscRFR.param_grid))
 params_df = pd.DataFrame(list(ParameterGrid(gscRFR.param_grid)))
 params_df.index.name = "index"
 params_df.to_csv("short_params.tsv", sep = "\t")
+params_indices = pd.DataFrame(params_df.index)
+params_indices.to_csv("short_params_indices", sep = "\t", index = False)
